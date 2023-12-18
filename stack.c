261a3c0258647436fe160fcc4585df4da1ed3dbf
Matprod <matprod.matheo@sfr.fr>
2023-12-18 11:10:10 +0100

    restarting

--- stack.c
+++ stack.c
@@ -6,7 +6,7 @@
 /*   By: Matprod <matprod42@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 19:43:49 by Matprod           #+#    #+#             */
-/*   Updated: 2023/12/13 19:58:51 by Matprod          ###   ########.fr       */
+/*   Updated: 2023/12/15 18:33:29 by Matprod          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,28 +44,61 @@ t_stack *new_stack(int value)
 
     return new;
 }
-void	*fill_stack(t_stack **stack, t_stack *new_node)
+
+void stack_add_back(t_stack **stack, t_stack *new_stack)
 {
-	
+	if(!new_stack)
+		return ;
+	if(!*stack)
+	{
+		*stack = new_stack;
+		return ;
+	}
+	t_stack *temp;
+
+	temp = *stack;
+	while(temp->next)
+		temp = temp->next;
+	temp->next = new_stack;
+
+}
+void print_stack(t_stack *stack)
+{
+    while (stack)
+	{
+        printf("[%d]\n", stack->value);
+        stack = stack->next;
+    }
+    printf("\n");
+}
+
+void free_stack(t_stack *stack)
+{
+    while (stack)
+	{
+        t_stack* temp = stack;
+        stack = stack->next;
+        free(temp);
+    }
 }
 
 int main()
 {
+    t_stack *stack = NULL;
 
-    int value = 42;
-    t_stack* stack = new_stack(value);
+    t_stack *node1 = new_stack(1);
+    t_stack *node2 = new_stack(2);
+   // t_stack *node3 = new_stack(3);
 
-    if (stack != NULL)
-	{
-        printf("Nouvel élément de pile créé avec la valeur : %d\n", stack->value);
-        free(new_stack);
-    }
-	else
-	{
-        printf("Erreur lors de la création du nouvel élément de pile.\n");
-    }
+    stack_add_back(&stack, node1);
+	print_stack(stack);
+	
+    stack_add_back(&stack, node2);
+	print_stack(stack);
 
-    return 0;
+/*     stack_add_back(&stack, node3);
+    print_stack(stack); */
+
+	free(stack);
+	return (0);
 }
-
-
